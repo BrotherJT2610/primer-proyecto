136516a5875da616037425a1a90c224d41a320f8
BrotherJT2610 <julianmoscon@gmail.com>
2024-05-30 10:20:43 -0300

    pipipi

---
+++ src/app/modules/autentificacion/pages/registro/registro.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent {
+  //este "hide" es para el input de contraseña
+  hide = true
+
+  // IMPORTACION DEL MODELO / INTERFAZ
+  usuarios: Usuario = {
+    uid: "",
+    nombre: "",
+    apellido: "",
+    email: "",
+    rol: "",
+    password: "",
+  }
+
+  //crear una coleccion para usuarios
+  coleccionUsuarios: Usuario[] = []
+
+
+  //funcion para el usuario
+  registrar() {
+    const credenciales = {
+      uid: this.usuarios.uid,
+      nombre: this.usuarios.nombre,
+      apellido: this.usuarios.apellido,
+      email: this.usuarios.email,
+      rol: this.usuarios.rol,
+      password: this.usuarios.password,
+    }
+
+    //enviamos los nuevos registros por medio del metodo push a la coleccion 
+    this.coleccionUsuarios.push(credenciales);
+
+    //por consola
+    console.log(credenciales)
+    console.log(this.coleccionUsuarios)
+
+  }
+}
