1549ae7e2f5c3ad6127f098e89c70e64011a9649
BrotherJT2610 <julianmoscon@gmail.com>
2024-11-08 09:56:13 -0300

    Creacion de modelo, servicio y componente necesarios para carrito

---
+++ src/app/models/pedido.ts
@@ -0,0 +1,7 @@
+import { Producto } from "./producto";
+export interface Pedido {
+    idPedido:string;
+    producto:Producto;
+    cantidad:number;
+    total:number;
+}
---
+++ src/app/modules/carrito/services/service.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { CrudService } from '../../admin/services/crud.service';
+import { AuthService } from '../../autentificacion/services/auth.service';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Pedido } from 'src/app/models/pedido';
+import { map } from 'rxjs';
+import { Producto } from 'src/app/models/producto';
+@Injectable({
+  providedIn: 'root'
+})
+export class CarritoService {
+  pedido:Pedido = {
+    idPedido:'',
+    producto:{
+      idProducto:'',
+      nombre:'',
+      precio:0,
+      descripcion:'',
+      categoria:'',
+      imagen:'',
+      alt:''
+    },
+    cantidad:0,
+    total:0
+  }
+  private pedidosColeccion: AngularFirestoreCollection<Pedido>
+  private uid: string | null = null;
+  constructor(
+    private servicioCrud:CrudService,
+    private servicioAuth:AuthService,
+    private servicioFirestore:AngularFirestore
+  ) {
+    //Creamos un subcoleccion dentro de la coleccion de usuarios y le damos ese valor a pedidosColeccion
+    this.pedidosColeccion = this.servicioFirestore.collection(`usuarios/${this.uid}/pedido`);
+  }
+  //Funcion para inicializar el carrito
+  iniciarCart(){
+    this.servicioAuth.obtenerUid().then(uid => {
+      //Obtenemos el ID del usuario para la subcoleccion
+      this.uid = uid
+      if(this.uid){
+        console.log(this.uid);
+      }else{
+        console.error('No se obtuvo el UID');
+      }
+    });
+  }
+  obtenerCarrito(){
+    return this.pedidosColeccion.snapshotChanges().pipe(map(action => action.map(a => a.payload.doc.data())));
+  }
+  crearPedido(producto:Producto, stock:number){
+    try {
+      //Creamos un ID para el pedido que sera subido
+      const idPedido = this.servicioFirestore.createId();
+      //Reemplazamos los valores de pedido por los valores que obtuvimos
+      this.pedido.idPedido = idPedido;
+      this.pedido.producto = producto;
+      this.pedido.cantidad = stock;
+      this.pedido.total = producto.precio*stock;
+      
+    } catch (error) {
+      
+    }
+  }
+}
